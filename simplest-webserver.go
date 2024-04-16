413823cf18367894f80e6d509e25bb04d1979304
Janis Gailis <janis.gailis@uia.no>
2024-04-16 17:56:52 +0200

    endret port

--- simplest-webserver.go
+++ simplest-webserver.go
@@ -6,6 +6,6 @@ import (
 func main() {
   http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path) })
-  http.ListenAndServe(":8080", nil)
+  http.ListenAndServe(":8088", nil)
 
 }
